869ebcfe9f61deae7ac67b77d05c9aca20b45c9b
livingston477 <93758347+livingston477@users.noreply.github.com>
2022-07-01 14:05:42 +0530

    second commit

---
+++ reverse_string.py
@@ -0,0 +1,13 @@
+# python reverse  string
+
+txt ="\nHello World"[::-1] # way is use a slice that steps backwards
+
+print(txt)
+
+# creating a function 
+def my_function(x):
+     return x[::-1]
+
+mytxt= my_function("\ni wonder how this text loos like backward")
+
+print(mytxt)
